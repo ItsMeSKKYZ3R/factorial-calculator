11f99fd0b73a8d4a36c09f471d20fd27f496212b
ItsMeSKKYZ3R <71552277+ItsMeSKKYZ3R@users.noreply.github.com>
2022-06-12 10:49:55 +0200

    Update get_user_input function and add clear function.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
-use std::io;
+use std::{io, process::Command};
 
-fn get_user_input() -> String {
+fn clear() {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cls")
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("clear")
+            .output()
+            .expect("failed to execute process")
+    };
+
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+}
+
+fn get_user_input(question: &str) -> String {
+    println!("{}", question);
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    input
+    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    input.trim().parse::<String>().unwrap()
 }
 
 fn main() {
+    clear();
 
+    let factorial_type = get_user_input("What type of factorial do you want to calculate? Simple / Complex\n");
+
+    clear();
+
+    match factorial_type.to_lowercase().as_str() {
+        "simple" => {
+            todo!()
+        },
+        "complex" => {
+            todo!()
+        },
+        _ => {
+            println!("Invalid input");
+        }
+    }
 }
