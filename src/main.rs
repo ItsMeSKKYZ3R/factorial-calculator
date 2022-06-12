22d5712335b9b531d3cf9ca23b72ebe05e20d943
ItsMeSKKYZ3R <71552277+ItsMeSKKYZ3R@users.noreply.github.com>
2022-06-12 10:34:12 +0200

    Getting the user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
+fn get_user_input() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    input
+}
+
 fn main() {
 
 }
